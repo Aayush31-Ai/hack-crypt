41736973c4b64aa42c11223576d1c3b5a9e42313
sujal jaiswal <sujaljaiswal.3734@gmail.com>
2026-01-17 03:07:19 +0530

    2nd game

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,11 @@ import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Profile from './components/Profile/Profile.jsx'
 import Store from './components/Store/Store.jsx'
-import Mario from './games/mario.jsx'
+import Mario from './games/Mario.jsx'
 import Login from './components/Login'
-import Testing from './components/Test/Testing.jsx'
-import Testing2 from './components/Test/Testing2.jsx'
-import Login from './components/Login.jsx'
+// import Testing from './components/Test/Testing.jsx'
+// import Testing2 from './components/Test/Testing2.jsx'
+// import Login from './components/Login.jsx'
 // import Home from './components/Home.jsx'
 import LandingPage from './components/home/LandingPage'
 import WorldDetail from './pages/WorldDetail'
@@ -17,7 +17,7 @@ import QuizPage from './pages/QuizPage'
 import Worlds from './pages/World'
 import ChallengePage from './pages/ChallangePage'
 import Leaderboard from './components/LeaderBoard/LeaderBoard'
-import BoardData from './components/LeaderBoard/BoardData'
+// import BoardData from './components/LeaderBoard/BoardData'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,12 +29,12 @@ const router = createBrowserRouter(
         <Route path="/login/:person" element={<Login />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="world" element={<Worlds />} />
-          <Route path="/world/:worldId" element={<WorldDetail />} />
-          <Route path="/world/:worldId/zone/:zoneId/stage/:stageId" element={<QuizPage />} />
-          <Route path="/challenge" element={<ChallengePage />} />
+        <Route path="/world/:worldId" element={<WorldDetail />} />
+        <Route path="/world/:worldId/zone/:zoneId/stage/:stageId" element={<QuizPage />} />
+        <Route path="/challenge" element={<ChallengePage />} />
         <Route path="/login/:person" element={<Login />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="world" element={<World />} />
+        <Route path="world" element={<Worlds />} />
         <Route path="/world/:worldId" element={<WorldDetail />} />
         <Route path="/world/:worldId/zone/:zoneId/stage/:stageId" element={<QuizPage />} />
         <Route path='/leaderboard' element={<Leaderboard />} />
