7f0075627b0cc8b3dd69c6d97ce94f8e2b99037a
Sushant78-CS <your_sushant_email@example.com>
2026-01-16 16:03:13 +0530

    Improved file structure

---
+++ frontend/src/components/Header/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <img src="" alt="logo" />
+                </div>
+                <div>
+                    <div>ClassRoom</div>
+                    <div>Practice</div>
+                </div>
+                <div>
+                    <div>
+                        Search
+                    </div>
+                    <div>Profile</div>
+                </div>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
